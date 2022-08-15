af1cd69744aa55faaa7709b037eae37083727776
Aleksandr Baranov <a.n.baranov@outlook.com>
2022-08-15 13:52:22 +0300

    chore: move storage to data folder

--- go.mod
+++ go.mod
@@ -10,7 +10,6 @@ require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
-	github.com/mitchellh/go-homedir v1.1.0
 	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
 	github.com/robfig/cron v1.2.0
 	golang.org/x/mod v0.3.0 // indirect
--- main.go
+++ main.go
@@ -3,21 +3,15 @@ package main
 import (
 	"log"
 	"os"
-	"path/filepath"
 	"share_bot/bot"
 	"share_bot/remind"
 	"share_bot/storage/db"
-
-	"github.com/mitchellh/go-homedir"
 )
 
-const dbFileName string = "share_bot.db"
+const dbPath string = "data/db/share_bot.db"
 const tokenEnv string = "SHARE_BOT_TELEGRAM_TOKEN"
 
 func main() {
-	home, _ := homedir.Dir()
-	dbPath := filepath.Join(home, dbFileName)
-
 	token, exists := os.LookupEnv(tokenEnv)
 	if !exists {
 		log.Fatal("telegram token does not exist")
@@ -31,5 +25,4 @@ func main() {
 
 	dsp := bot.NewDispatcher(token, storage)
 	log.Println(dsp.Poll())
-
 }
